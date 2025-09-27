887f02044892b0dce9499050567601ce210907b8
Bikram Panda <pandabikram687@gmail.com>
2025-09-27 09:46:47 +0530

    Use environment variable for geocoder email

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,7 +1,14 @@
+// // utils/geocoder.js
 const NodeGeocoder = require("node-geocoder");
 
 const options = {
   provider: "openstreetmap",   // free geocoding
+  httpAdapter: "https",         // use https
+  formatter: null,              // default formatting
+  // Add custom HTTP headers to comply with OSM's usage policy
+  headers: {
+    "User-Agent": `WanderlustApp/1.0 (${process.env.GEOCODER_EMAIL})`
+  },
 };
 
 const geocoder = NodeGeocoder(options);
